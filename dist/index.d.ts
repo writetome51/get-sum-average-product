7534c29bdd285a80408889964e2d83428b0a91e6
Steve Thompson <writetome51@gmail.com>
2020-07-28 00:28:42 -0600

    Committing version 2.0.0

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function getSum(numbers: number[]): number;
-
-
-export declare function getProduct(numbers: number[]): number;
-
-
-export declare function getAverage(numbers: number[]): number;
--- tests.js
+++ tests.js
@@ -1,4 +1,4 @@
-import { getAverage, getProduct, getSum } from './index.js';
+import { getAverage, getProduct, getSum } from 'index.js';
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let sum = getSum(nums);
 if (sum === 55)
