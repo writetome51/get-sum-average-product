65f2970a5aa45d3f8f8fed09ff1e9078809162be
Steve Thompson <writetome51@gmail.com>
2020-07-28 00:32:37 -0600

    Re-committing version 2.0.0

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-import { errorIfNotFiniteNumber } from 'error-if-not-finite-number';
-import { errorIfLengthIsZero } from 'error-if-length-is-zero';
+import {errorIfNotFiniteNumber} from 'error-if-not-finite-number';
+import {errorIfLengthIsZero} from 'error-if-length-is-zero';
+
+
 export function getSum(numbers) {
-    errorIfLengthIsZero(numbers);
-    for (var i = 0, total = 0; i < numbers.length; ++i) {
-        errorIfNotFiniteNumber(numbers[i]);
-        total += numbers[i];
-    }
-    return total;
+	errorIfLengthIsZero(numbers);
+
+	for (var i = 0, total = 0; i < numbers.length; ++i) {
+		errorIfNotFiniteNumber(numbers[i]);
+		total += numbers[i];
+	}
+	return total;
 }
+
+
 export function getProduct(numbers) {
-    errorIfLengthIsZero(numbers);
-    for (var i = 0, product = 1; i < numbers.length; ++i) {
-        errorIfNotFiniteNumber(numbers[i]);
-        product *= numbers[i];
-    }
-    return product;
+	errorIfLengthIsZero(numbers);
+
+	for (var i = 0, product = 1; i < numbers.length; ++i) {
+		errorIfNotFiniteNumber(numbers[i]);
+		product *= numbers[i];
+	}
+	return product;
 }
+
+
 export function getAverage(numbers) {
-    let sum = getSum(numbers);
-    return (sum / numbers.length);
+	let sum = getSum(numbers);
+	return (sum / numbers.length);
 }
--- tests.js
+++ tests.js
@@ -1,36 +1,49 @@
-import { getAverage, getProduct, getSum } from 'index.js';
+import { getAverage, getProduct, getSum } from './index.js';
+
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let sum = getSum(nums);
 if (sum === 55)
     console.log('test 1 passed');
 else
     console.log('test 1 FAILED');
+
+
 let product = getProduct(nums);
 if (product === 3628800)
     console.log('test 2 passed');
 else
     console.log('test 2 FAILED');
+
+
 let avg = getAverage(nums);
 if (avg === 5.5)
     console.log('test 3 passed');
 else
     console.log('test 3 FAILED');
+
+
 nums = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -1];
 sum = getSum(nums);
 if (sum === -56)
     console.log('test 4 passed');
 else
     console.log('test 4 FAILED');
+
+
 product = getProduct(nums);
 if (product === -3628800)
     console.log('test 5 passed');
 else
     console.log('test 5 FAILED');
+
+
 avg = getAverage(nums);
 if (avg === -5.090909090909091)
     console.log('test 6 passed');
 else
     console.log('test 6 FAILED');
+
+
 nums = [-10];
 console.log(getSum(nums));
 console.log(getProduct(nums));
