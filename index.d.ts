21b102ac7f9d86355591282fbd6386c040d66807
Steve Thompson <writetome51@gmail.com>
2021-05-12 11:18:17 -0600

    Added typescript declarations

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export declare function getSum(numbers: number[]): number;
+
+export declare function getProduct(numbers: number[]): number;
+
+export declare function getAverage(numbers: number[]): number;
